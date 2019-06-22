7c2720114d80907cbe25aab67edba63b67c300a6
agustinasanchez <agustinabelen.sanchez@gmail.com>
2019-06-21 21:41:17 -0300

    fracasando

---
+++ js/script.js
@@ -0,0 +1,42 @@
+var franchise = {
+    
+    sellers: ["Ada", "Grace", "Hedy", "Sheryl"],
+  
+    sale: [
+      // tener en cuenta que Date guarda los meses del 0 (enero) al 11 (diciembre)
+      { date: new Date(2019, 1, 4), seller: "Grace", components: ["Monitor GPRS 3000", "Motherboard ASUS 1500"] },
+      { date: new Date(2019, 0, 1), seller: "Ada", components: ["Monitor GPRS 3000", "Motherboard ASUS 1500"] },
+      { date: new Date(2019, 0, 2), seller: "Grace", components: ["Monitor ASC 543", "Motherboard MZI"] },
+      { date: new Date(2019, 0, 10), seller: "Ada", components: ["Monitor ASC 543", "Motherboard ASUS 1200"] },
+      { date: new Date(2019, 0, 12), seller: "Grace", components: ["Monitor GPRS 3000", "Motherboard ASUS 1200"] }
+    ],
+  
+    prices: [
+      { component: "Monitor GPRS 3000", price: 200 },
+      { component: "Motherboard ASUS 1500", price: 120 },
+      { component: "Monitor ASC 543", price: 250 },
+      { component: "Motherboard ASUS 1200", price: 100 },
+      { component: "Motherboard MZI", price: 30 },
+      { component: "HDD Toyiva", price: 90 },
+      { component: "HDD Wezter Dishital", price: 75 },
+      { component: "RAM Quinston", price: 110 },
+      { component: "RAM Quinston Fury", price: 230 }
+    ]
+}
+
+//1. precioMaquina
+let salePrices = sale => {
+    salePrices = 0
+    sale.forEach(e => {
+        const component = e.prices.find (({item}) => e === item)
+        salePrices = salePrices + component.price
+        return salePrices 
+    });
+ 
+ const venta = ["Monitor GPRS 3000", "Motherboard ASUS 1500"];
+ 
+ console.log (salePrices(venta));
+}
+
+//2.
+
