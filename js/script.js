076b6a058b3f164d4ce7881d120a3b984a6de473
garemar <gabrielamartinez0908@hotmail.com>
2019-06-30 20:45:08 -0300

    fn 1c,2a y styles

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
 var franchise = {
     
     sellers: ["Ada", "Grace", "Hedy", "Sheryl"],
+
+    branch: ['Centro', 'Caballito'],
     
-    componentsSold: [
-      // tener en cuenta que Date guarda los meses del 0 (enero) al 11 (diciembre)
-      { date: new Date(2019, 1, 4), seller: "Grace", components: ["Monitor GPRS 3000", "Motherboard ASUS 1500"] },
-      { date: new Date(2019, 0, 1), seller: "Ada", components: ["Monitor GPRS 3000", "Motherboard ASUS 1500"] },
-      { date: new Date(2019, 0, 2), seller: "Grace", components: ["Monitor ASC 543", "Motherboard MZI"] },
-      { date: new Date(2019, 0, 10), seller: "Ada", components: ["Monitor ASC 543", "Motherboard ASUS 1200"] },
-      { date: new Date(2019, 0, 12), seller: "Grace", components: ["Monitor GPRS 3000", "Motherboard ASUS 1200"] }
+    sales: [
+      { date: new Date(2019, 1, 4), seller: "Grace", components: ["Monitor GPRS 3000", "Motherboard ASUS 1500"], branch:'Centro'}, 
+      { date: new Date(2019, 0, 1), seller: "Ada", components: ["Monitor GPRS 3000", "Motherboard ASUS 1500"], branch:'Centro'},
+      { date: new Date(2019, 0, 2), seller: "Grace", components: ["Monitor ASC 543", "Motherboard MZI"],  branch:'Centro'},
+      { date: new Date(2019, 0, 10), seller: "Ada", components: ["Monitor ASC 543", "Motherboard ASUS 1200"],  branch:'Centro'},
+      { date: new Date(2019, 0, 12), seller: "Grace", components: ["Monitor GPRS 3000", "Motherboard ASUS 1200"], branch:'Centro'},
+      { date: new Date(12/02/2019), seller: "Hedy", components: ["Monitor GPRS 3000", "HDD Toyiva"], branch:'Centro'},
+      { date: new Date(24/02/2019), seller: "Sheryl", components: ["Motherboard ASUS 1500", "HDD Wezter Dishital"], branch:'Caballito'},
+      { date: new Date(01/02/2019), seller: "Ada", components: ["Motherboard MZI", "RAM Quinston Fury"], branch:'Centro'},
+      { date: new Date(11/02/2019), seller: "Grace", components: ["Monitor ASC 543", "RAM Quinston"], branch:'Caballito'},
+      { date: new Date(15/02/2019), seller: "Ada", components: ["Motherboard ASUS 1200", "RAM Quinston Fury"], branch:'Centro'},
+      { date: new Date(12/02/2019), seller: "Hedy", components:["Motherboard ASUS 1500", "HDD Toyiva"], branch:'Caballito'},
+      { date: new Date(21/02/2019), seller: "Grace", components: ["Motherboard MZI", "RAM Quinston"], branch:'Centro'},
+      { date: new Date(08/02/2019), seller: "Sheryl", components: ["Monitor ASC 543", "HDD Wezter Dishital"], branch:'Centro'},
+      { date: new Date(16/02/2019), seller: "Sheryl", components: ["Monitor GPRS 3000", "RAM Quinston Fury"], branch:'Centro'},
+      { date: new Date(27/02/2019), seller: "Hedy", components: ["Motherboard ASUS 1200", "HDD Toyiva"], branch:'Caballito'},
+      { date: new Date(22/02/2019), seller: "Grace", components: ["Monitor ASC 543", "HDD Wezter Dishital"], branch:'Centro'},
+      { date: new Date(05/02/2019), seller:  "Ada", components: ["Motherboard ASUS 1500", "RAM Quinston"], branch:'Centro'},
+      { date: new Date(01/02/2019), seller: "Grace", components: ["Motherboard MZI", "HDD Wezter Dishital"], branch:'Centro'},
+      { date: new Date(07/02/2019), seller: "Sheryl", components: ["Monitor GPRS 3000", "RAM Quinston"], branch:'Caballito'},
+      { date: new Date(14/02/2019), seller: "Ada", components: ["Motherboard ASUS 1200", "HDD Toyiva"], branch:'Centro'},
     ],
 
     prices: [
@@ -25,11 +41,12 @@ var franchise = {
   
 };
 
+
 //1-a
 
-const machinePrice = sold => {
+const machinePrice = components => {
   let sumComponents = 0
-  sold.forEach(e => {
+  components.forEach(e => {
     const piece = franchise.prices.find(({component}) => e === component)
     sumComponents = sumComponents + piece.price
   })
@@ -37,14 +54,14 @@ const machinePrice = sold => {
 } 
 
 console.log(machinePrice(["Monitor ASC 543", "Motherboard MZI" ]))
-  
+
 //1-b
 
-const quantitySalesComponent = (c) =>{
+const quantitySalesComponent = (component) =>{
   let count = 0
-  franchise.componentsSold.forEach(e => {
-    e.components.forEach(piece => {
-      if(piece === c){
+  franchise.sales.forEach(sale => {
+    sale.components.forEach(piece => {
+      if(piece === component){
         count++
       }
     })
@@ -55,15 +72,57 @@ const quantitySalesComponent = (c) =>{
 console.log(quantitySalesComponent("Monitor ASC 543"))
 
 //1-c 
-    
+
+/*const bestSellerMonth = (month, year) => {
+  const sales = franchise.sales;
+  const sellers = franchise.sellers;
+
+  const fileteredSales = sales.filter(
+    sale => sale.date.getMonth() + 1 === month && sale.date.getFullYear() === year
+  );
+
+  const salesBySeller = sellers.map(seller => {
+    return {
+      seller: seller,
+      totalAmount: fileteredSales.reduce((accum, curr) => { 
+        if (seller === curr.seller) {
+          return accum + machinePrice(curr.components);
+        } else {
+          return accum;
+        }
+      }, 0)
+    };
+  });
+
+  //(condicion) ? siSeCumpleHagoEsto : siNoSeCumpleHagoEstoOtro;
+
+  return salesBySeller
+    .reduce((accum, curr) => (curr.totalAmount > accum.totalAmount ? curr : accum), { seller: "", totalAmount: 0 });
+};
+
+
+console.log('best seller was: ' + bestSellerMonth(2, 2019).seller + ', with sales for: ' + bestSellerMonth(2, 2019).totalAmount);*/
+
+const bestSellerMonth = (month, year) => {
+  const fileteredSales = franchise.sales.filter(sale => sale.date.getMonth() + 1 === month && sale.date.getFullYear() === year);
+  return franchise.sellers
+    .map(seller => ({
+      seller: seller,
+      totalAmount: fileteredSales.reduce((accum, curr) => (seller === curr.seller) ? accum + machinePrice(curr.components) : accum, 0)
+    }))
+    .reduce((accum, curr) => (curr.totalAmount > accum.totalAmount ? curr : accum), { seller: "", totalAmount: 0 })
+    .seller;
+}
+
+console.log('best seller was: ' + bestSellerMonth(2, 2019));
 
 //1-d
 
  const salesMonth = (month, year) => {
   let sumMonth = 0
-  for (let index = 0; index < franchise.componentsSold.length; index++) {
-      let components = franchise.componentsSold[index].components
-      if (franchise.componentsSold[index].date.getMonth() + 1 == month && franchise.componentsSold[index].date.getFullYear() == year) {
+  for (let index = 0; index < franchise.sales.length; index++) {
+      let components = franchise.sales[index].components
+      if (franchise.sales[index].date.getMonth() + 1 == month && franchise.sales[index].date.getFullYear() == year) {
           sumMonth = sumMonth + machinePrice(components)
       }
 
@@ -76,7 +135,7 @@ console.log(salesMonth(1, 2019)); // 1250
 
 const salesBySeller = name => {
   let sellingSum = 0
-  franchise.componentsSold.forEach(e => {
+  franchise.sales.forEach(e => {
     if(e.seller === name){
       let totalSales = machinePrice(e.components)
       sellingSum += totalSales
@@ -87,6 +146,23 @@ const salesBySeller = name => {
 }
 console.log(salesBySeller("Ada"));
 
+//1-f componenteMasVendido():
+
+const bestSellerComponent = () => {
+  let sellingComponent = []
+  let nameComponent
+  franchise.prices.forEach(e => {
+    sellingComponent.push(quantitySalesComponent(e.component))
+    countMaxComponent = Math.max.apply(null, sellingComponent)
+    if(quantitySalesComponent(e.component) === countMaxComponent){
+    nameComponent = e.component
+    }
+  })
+  return nameComponent
+}
+
+console.log(bestSellerComponent() ); // Monitor GPRS 3000
+
 //1-g
 
 const wereThereSales = (month, year) => {
@@ -94,6 +170,31 @@ const wereThereSales = (month, year) => {
   }
   
   console.log('hubo ventas?:' , wereThereSales(3, 2019));
+
+  for (let index = 0; index < franchise.sales.length; index++) {
+    franchise.sales[index].sucursal = 'Centro'
+
+}
+
+console.log(franchise.sales)
+
+franchise.sales.forEach(sold => {
+  sold.branch = 'Centro';
+});
+console.log(franchise.sales);
+
+// 2-a
+const salesForBranch = (branch) => {
+  const sales = franchise.sales;
+  return sales
+    .filter(sale => sale.branch === branch)
+    .reduce((accum, curr) => accum = accum + machinePrice(curr.components), 0);
+};
+
+console.log('sales for branch Centro', salesForBranch('Centro'));
+
+
+
 
 //3-a
   const renderPerMonth = () => {
@@ -111,3 +212,5 @@ const wereThereSales = (month, year) => {
     }
     
     renderPerMonth()
+
+    
